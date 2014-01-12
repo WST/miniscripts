2320d6fd68578a14faba4c9f652a9462e574a7cd
WST <admin@jsmart.web.id>
2014-01-12 19:27:31 +0400

    Ability to retry fetching data

--- munin-plugins/tasvideos.php
+++ munin-plugins/tasvideos.php
@@ -10,7 +10,20 @@ if(!isset($argv)) {
 	die('This script should be called from command line');
 }
 
+$graph_caption = 'Sonic tool-assisting speedrunners';
 $players = ['marzojr', 'WST'];
+$retries = 1;
+
+function getPoints($username) {
+	do {
+		$data = @ file_get_contents('http://tasvideos.org/playerinfo/' . urlencode($username) . '.json');
+		if($data) {
+			$information = @ json_decode($data);
+			if($information === false) continue;
+			return $information->points;
+		}
+	} while($retries --);
+}
 
 function encodeName($username) {
 	return str_replace(' ', '_', strtolower($username));
@@ -19,21 +32,17 @@ function encodeName($username) {
 if(@ $argv[1]){
 	$label = 'points';
 	if(!(rand() % 10)) $label = 'ponies';
-	echo "graph_title TASvideos expert players\ngraph_vlabel {$label}\ngraph_category TASvideos\n";
+	echo "{$graph_caption}\ngraph_vlabel {$label}\ngraph_category tasvideos\n";
 	foreach($players as $player) {
 		echo encodeName($player). ".label {$player}\n";
 	}
 	die();
 }
 
+@ ini_set('default_socket_timeout', 30);
+
 foreach($players as $player) {
-	$data = @ file_get_contents('http://tasvideos.org/playerinfo/' . urlencode($player) . '.json');
-	if($data === false) {
-		// Handle the error
-		continue;
-	} else {
-		$information = json_decode($data);
-		$player_name = encodeName($player);
-		echo "{$player_name}.value {$information->points}\n";
-	}
+	$player_name = encodeName($player);
+	$player_points = getPoints($player);
+	echo "{$player_name}.value {$player_points}\n";
 }
