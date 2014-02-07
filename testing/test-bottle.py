57ffc94a72f4a753ef8f92b31874ecd0da389832
WST <admin@jsmart.web.id>
2014-02-07 23:03:31 +0400

    cosmetic editions

--- testing/test-bottle.py
+++ testing/test-bottle.py
@@ -4,6 +4,6 @@ from bottle import route, run
 
 @route('/hello/<name>')
 def hello(name):
-    return '<h1>Hello %s!</h1>' % name.title()
+	return '<h1>Hello %s!</h1>' % name.title()
 
 run(host='localhost', port=8080)
