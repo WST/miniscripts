37342e329adf3d9ce22ca229b0023d79285a6aaf
WST <admin@jsmart.web.id>
2015-02-23 15:24:04 +0300

    Luvit test

---
+++ testing/test-luvit.lua
@@ -0,0 +1,14 @@
+#!/usr/bin/lua
+
+local http = require("http")
+
+http.createServer(function (req, res)
+	local body = "Hello world\n"
+	res:writeHead(200, {
+		["Content-Type"] = "text/plain",
+		["Content-Length"] = #body
+	})
+	res:finish(body)
+end):listen(8080)
+
+print("Server listening at http://localhost:8080/")
