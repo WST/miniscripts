3951fb92680061c52bace479c23089a7a7f296d7
WST <ilya.averkov@gmail.com>
2016-04-27 21:55:44 +0300

    + threading

--- testing/python-async-server/server.py
+++ testing/python-async-server/server.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-import asyncore
+import asyncore, threading
+
+# Создадим мьютекс
+mutex = threading.Lock()
 
 # Модуль asyncore на данный момент считается устаревшим
 # https://docs.python.org/3.5/library/asyncore.html
@@ -28,8 +31,19 @@ class EchoServer(asyncore.dispatcher):
 		self.listen(5)
 
 	def handle_accepted(self, sock, addr):
+		mutex.acquire()
 		print('Incoming connection from %s' % repr(addr))
 		handler = EchoHandler(sock)
+		mutex.release()
 
-server = EchoServer('localhost', 8080)
-asyncore.loop()
+server = EchoServer('localhost', 8888)
+
+worker_1 = threading.Thread(target = asyncore.loop)
+worker_2 = threading.Thread(target = asyncore.loop)
+
+worker_1.start()
+worker_2.start()
+
+worker_1.join()
+worker_2.join()
+
